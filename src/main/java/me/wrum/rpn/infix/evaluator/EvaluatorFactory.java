3d29a6f5bce2eea2ebc97b6f536091ae7185f6d6
Vyacheslav Gusser <proweber1@mail.ru>
2021-11-06 21:29:17 +0300

    chore: javadoc for EvaluatorFactory

--- src/main/java/me/wrum/rpn/infix/evaluator/EvaluatorFactory.java
+++ src/main/java/me/wrum/rpn/infix/evaluator/EvaluatorFactory.java
@@ -4,11 +4,21 @@ import me.wrum.rpn.api.evaluator.Evaluator;
 import me.wrum.rpn.infix.lexer.LexerFactory;
 import me.wrum.rpn.infix.operator.OperatorsSupportFactory;
 
+/**
+ * Factory for creating instances of evaluators
+ *
+ * @author Vyacheslav Gusser proweber1@mail.ru
+ */
 public final class EvaluatorFactory {
   private EvaluatorFactory() {
     throw new UnsupportedOperationException();
   }
 
+  /**
+   * Creates a fully configured evaluator for evaluating infix expressions
+   *
+   * @return evaluator instance
+   */
   public static Evaluator create() {
     return new RpnEvaluator(
         LexerFactory.initLexer(),
